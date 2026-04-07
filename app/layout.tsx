f5ae0871eb094bd2d94f744eb23b8752d3a0fcdb
Vivekananda Padhi <vivekanandapadhi8249@gmail.com>
2026-04-07 14:46:54 +0530

    initial commit project setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const dmSans = DM_Sans({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-dm-sans",
 });
 
 export const metadata: Metadata = {
@@ -23,11 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`${dmSans.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col font-sans">
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <div>
-    <h2>Hello Next Js </h2>
-   </div>
+    <div>
+      <h2>Hello Next Js</h2>
+      <Button>Search</Button>
+    </div>
   );
 }
