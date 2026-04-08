b7fb1e88c64d55c1e8cb188785f39e6e0d8a4111
Vivekananda Padhi <vivekanandapadhi8249@gmail.com>
2026-04-08 15:03:46 +0530

    database setup

---
+++ config/schema.tsx
@@ -0,0 +1,8 @@
+import { integer, pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull(),
+    credits: integer("credit").default(10),
+});
---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+    schema: "./config/schema.tsx",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+});
